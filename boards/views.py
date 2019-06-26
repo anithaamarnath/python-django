c452063de1c69b408902c5c516dc751f5e10a27d
anithaamarnath <anithaamarnath04@gmail.com>
2019-06-26 14:10:09 -0700

    first time render html content

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, world!')
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
