a5f964d8a121fc09a39824687b9688f830d659e5
anithaamarnath <anithaamarnath04@gmail.com>
2019-06-27 19:12:45 -0700

    final commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,5 @@ from boards import views
 urlpatterns = [
   url(r'^$',views.home, name='home'),
   url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-  url(r'^admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
 ]
